3a96030e8e8a2791b0652b1a3b46d6f14c514213
NarendraDhamecha <nikhildhamecha19@gmail.com>
2022-11-22 20:30:43 +0530

    task 5

---
+++ task_5.js
@@ -0,0 +1,9 @@
+let item = document.getElementsByTagName("li");
+
+item[4].style.color = 'red';
+
+let item_5 = document.getElementsByClassName("list item");
+
+item_5[0].style.backgroundColor = 'lightblue';
+item_5[0].style.fontSize = '20px';
+item_5[0].style.fontWeight ='bold';
