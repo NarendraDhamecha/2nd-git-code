2057ede4a12c3f4dabc6f946d62ae692e7ccd5c9
NarendraDhamecha <nikhildhamecha19@gmail.com>
2022-11-22 16:02:54 +0530

    practice_1

---
+++ practice_1.js
@@ -0,0 +1,13 @@
+//console.log(document.all);
+
+let head= document.getElementById('main-header');
+//console.log(head);
+
+//head.innerText='nikhil';
+
+head.style.borderBottom='solid 3px #000';
+
+let title= document.getElementById('title');
+//console.log(title);
+
+title.style.color='green';
