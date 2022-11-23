2592971ad7a3860d743ff6d860cdecc1b9afbccb
NarendraDhamecha <nikhildhamecha19@gmail.com>
2022-11-23 16:46:14 +0530

    Task 8

---
+++ task_8.js
@@ -0,0 +1,55 @@
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+
+
+form.addEventListener('submit', addItem);
+
+itemList.addEventListener('click', removeItem);
+
+filter.addEventListener('keyup', filterItems);
+
+
+function addItem(e){
+  e.preventDefault();
+
+ 
+  var newItem = document.getElementById('item').value;
+
+  
+  var li = document.createElement('li');
+  
+  li.className = 'list-group-item';
+  
+  li.appendChild(document.createTextNode(newItem));
+
+  
+  var deleteBtn = document.createElement('button');
+
+ 
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+ 
+  deleteBtn.appendChild(document.createTextNode('X'));
+
+  
+  li.appendChild(deleteBtn);
+
+  var editbtn = document.createElement('button');
+  editbtn.style.cssFloat='right';
+  editbtn.appendChild(document.createTextNode('edit'));
+  li.appendChild(editbtn);
+
+ 
+  itemList.appendChild(li);
+}
+
+
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
+
